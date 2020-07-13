7bd7aebc6bf4c94503de76d6214b10c55e94b2f1
Wolfgang Schubert <david.rodriguez@agileinnova.org>
2020-07-13 00:50:08 -0500

    add json file

--- index.js
+++ index.js
@@ -22,13 +22,17 @@ app.use(express.urlencoded({extended: false}));
 app.use(cors());
 
 
-app.use('/api/',routes);
-//app.use(require('./routes/routes'));
+
 
 
 // Static files
-//app.use('/public', express.static(path.join(__dirname, './public')));
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
+
+app.use('/api/',routes);
+//app.use(require('./routes/routes'));
 
 
 app.listen(app.get("port"), ()=>{
--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,32 @@
 const { Router, response } = require('express');
+const path = require('path');
 const router = Router();
 
 
 router.get('/',(req,res)=>{
-
   res.send('<h1>Hello World!</h1>');
+});
 
+
+router.post('/save',(req,res)=>{
+
+  console.log(req.body);
+
+  res.json(req.body);
+
+});
+
+
+router.get('/one-row/:id',(req,res)=>{
+  const id    = req.params.id;
+  console.log(id);
+
+});
+
+
+router.get('/movies',(req,res)=>{
+  
+  res.sendFile(path.join(__dirname, '../public/json', 'bd.json'));
 
 });
 
